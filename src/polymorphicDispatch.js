3519f3f8e894824905f3ef820c25676506c09ffe
Wei Zhu <yesmeck@gmail.com>
2016-01-14 21:04:57 +0800

    fix thunk action dispatch

--- src/polymorphicDispatch.js
+++ src/polymorphicDispatch.js
@@ -3,8 +3,8 @@ import key from './key'
 export default function polymorphicDispatch(disptch, as) {
   return (action) => {
     if (typeof action === 'function') {
-      action = (_, getState) => {
-        action(polymorphicDispatch(dispatch, as), getState)
+      action = (disptch, getState) => {
+        action(polymorphicDispatch(disptch, as), getState)
       }
     } else if (typeof action === 'object') {
       action[key] = as
